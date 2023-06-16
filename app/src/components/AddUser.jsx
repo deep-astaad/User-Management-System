31412cd982179ea84a75161446679801c9523fe2
Aman Deep Singh <deepamansinghads@gmail.com>
2023-06-16 18:36:16 +0530

    API Generation

--- app/src/components/AddUser.jsx
+++ app/src/components/AddUser.jsx
@@ -1,4 +1,7 @@
 import { FormGroup, FormControl, Button, InputLabel, Input, styled } from "@mui/material";
+import { useState } from "react";
+
+import { addUser } from "../service/api";
 
 const Container = styled(FormGroup)`
     width : 50%;
@@ -10,24 +13,47 @@ const Text = styled(FormControl)`
     margin-top : 20px;
 `;
 
+const defaultValue = {
+    name: '',
+    email: '',
+    username: '',
+    phone: ''
+}
+
 const AddUser = () =>{
+
+    const [user, setUser] = useState(defaultValue);
+
+    const onValueChange = (e) => {
+        console.log(e.target.name, e.target.value)
+        setUser({ ...user, [e.target.name]: e.target.value })
+        console.log(user)
+    }
+
+    const addUserDetails = async () => {
+        await addUser(user);
+    }
+
     return (
         <Container>
             <Text>
                 <InputLabel>Name</InputLabel>
-                <Input />
+                <Input onChange={(e) => onValueChange(e)} name = "name" />
             </Text>
             <Text>
                 <InputLabel>email</InputLabel>
-                <Input />
+                <Input onChange={(e) => onValueChange(e)} name = "email" />
             </Text>
             <Text>
                 <InputLabel>username</InputLabel>
-                <Input />
+                <Input onChange={(e) => onValueChange(e)} name = "username" />
             </Text>
             <Text>
                 <InputLabel>Phone</InputLabel>
-                <Input />
+                <Input onChange={(e) => onValueChange(e)} name = "phone" />
+            </Text>
+            <Text>
+                <Button variant="contained" onClick={() => addUserDetails()}>Add User</Button>
             </Text>
         </Container>
     )
